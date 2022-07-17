02ec88c0cfcc444b4fa976683a15e487c87ac664
NikitaSalunke10 <salunkenikita10@gmail.com>
2022-07-17 20:55:56 +0530

    Fibonacci Series program

--- Day6LogicalProblem/Program.cs
+++ Day6LogicalProblem/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int num1 = 0, num2 = 1, num3;
+Console.Write("Enter the number: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(num1);
+for(int i = 2; i <= num; i++)
+{
+    num3 = num1 + num2;
+    Console.WriteLine(num3);
+    num1 = num2;
+    num2 = num3;
+}
